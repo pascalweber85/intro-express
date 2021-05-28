e2a551a159b68abf219653a45b0864caf2c4c9f6
Pascal Weber <weber-pascal@web.de>
2021-05-28 12:22:35 +0200

    add routes folder with index.js and notes.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import express from 'express'
 import morgan from 'morgan'
+import routes from './routes/index.js'
 
 const app = express()
 
 app.use(morgan('dev'))
-
-app.get('/', function (req, res) {
-  res.send('hello')
-})
-
-app.get('/error', function (req, res) {
-  throw new Error('oops I made a mistake')
-})
+app.use(express.urlencoded({ extended: true }))
+app.use(routes)
 
 app.use(function (req, res) {
-  res.status(404).send('not found')
+  res.status(404).send('Not found')
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send(err.message)
 })
 
 app.listen(3000, function () {
-  console.log('app listening to port 3000')
+  console.log('app listening on port 3000')
 })
